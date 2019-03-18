ac1ea35d5daf1ef3ead6022061f649b8f18056ab
Daniel Castellanos <Castellanos2018*>
2019-03-18 17:00:08 -0500

    class number 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { fetchProducts } from './services/products'
+
 
 import FilterableProductTable from './components/FilterableProductTable.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productsRes: {
+        products: [],
+        errMess: null,
+        isLoading: true
+      }
+    }
+  }
+  componentDidMount() {
+    this.fetchProducts()
+  }
+
+  fetchProducts = async () => {
+    const response = await fetchProducts()
+
+    const productsRes = (response.error) ? {
+      products: [],
+      errMess: response.message,
+      isLoading: false
+    } : {
+      products: response,
+      errMess: null,
+      isLoading: false
+    }
+
+    this.setState({ productsRes })
+  }
+
   render() {
     return (
       <div className="App">
-        <FilterableProductTable />
+        <FilterableProductTable {...this.state.productsRes}/>
       </div>
     );
   }
--- src/components/dialog/Modal.js
+++ src/components/dialog/Modal.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 import Form from "./Form";
 
 class Modal extends Component {
+
   render() {
     return (
       <div>
-        <h1>ADD A PRODUCT</h1>
+        <h3>ADD A PRODUCT</h3>
         <p>
           Please fill the data requested in this dialog to add aproduct to our table <br/>
           including Product Name, Price and Category.
@@ -14,6 +15,7 @@ class Modal extends Component {
       </div>
     )
   }
+
 }
 
 export default Modal;
